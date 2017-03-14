1b677935fc69befa9f1567f19198302d2e82b7d3
josecervantesbim <jfcervantes.moreno@gmail.com>
2017-03-13 22:58:49 -0600

    Create solutionproblem1.js
    Definition of the problem

---
+++ solutionproblem1.js
@@ -0,0 +1,27 @@
+/*
+*  PROPELICS JS TEST by jfcervantes.moreno@gmail.com
+*  March 13
+*  For a test of specific problem please select the number of the problem to require the apropiate input.
+*/
+
+
+/*Write a function that takes a list of strings an prints them, one per line, in a rectangular frame. For example the array
+*  ["November", "is", "the", "coolest", "month", "of", "the", "Year"] gets printed as:
+*
+*
+      ************
+      * November *
+      * is *
+      * the *
+      * coolest *
+      * month *
+      * of *
+      * the *
+      * Year *
+      ************
+*
+*/
+function nombre_marco(listadostrings[])
+{
+
+}
