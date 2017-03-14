ad60c81a7b4d8e9b25a8c1a1bcae79182f28c718
josecervantesbim <jfcervantes.moreno@gmail.com>
2017-03-14 00:51:15 -0600

    Update solutionproblem1.js
    Solucion para ejecutar junto con solutionproblem1.html

--- solutionproblem1.js
+++ solutionproblem1.js
@@ -1,6 +1,6 @@
 /*
 *  PROPELICS JS TEST by jfcervantes.moreno@gmail.com
-*  March 13
+*  March 13 2017
 *  For a test of specific problem please select the number of the problem to require the apropiate input.
 */
 
@@ -21,7 +21,52 @@
       ************
 *
 */
-function nombre_marco(listadostrings[])
+function nombreMarco(listadoStrings)
 {
 
+var numeroStrings=listadoStrings.length;  //Obtenemos el numero de elementos en el array
+var mayorValorArray=0;						//Acumulador para obtener el string mas largo del arreglo
+var diferenciaString=0;						//Guardara la diferencia entre el mayor valor de array contra todos los demas
+var guardaEspacios="";						//Guarda el numero de espacios a guardar
+//Recorrido de ciclo para comparar 
+for (i=0; i<numeroStrings; i++)
+	{
+		console.log("Tamaño palabra:"+listadoStrings[i].length);
+	if(mayorValorArray<listadoStrings[i].length){mayorValorArray=listadoStrings[i].length}
+	}
+
+//Transformacion del arreglo  String igual a string mas espacios de valor arreglo
+for(i=0;i<numeroStrings;i++)
+	{
+		
+	if(listadoStrings[i].length<mayorValorArray){
+		diferenciaString=(mayorValorArray-listadoStrings[i].length);
+		console.log("Me faltan estos espacios:"+diferenciaString);	
+			guardaEspacios="";
+			for(j=0;j<diferenciaString;j++){
+				guardaEspacios=guardaEspacios+" ";
+			}
+		
+		listadoStrings[i]="*"+listadoStrings[i]+guardaEspacios+"*";
+		
+			}
+	else listadoStrings[i]="*"+listadoStrings[i]+"*";	
+	}	
+
+bordeAsterisco="";	
+//Crear bordes superior e inferior
+for (k=0;k<mayorValorArray;k++){
+	bordeAsterisco=bordeAsterisco+"*";
+	}
+//Correccion de espacios
+bordeAsterisco=bordeAsterisco+"**";	
+listadoStrings.unshift(bordeAsterisco);
+listadoStrings.push(bordeAsterisco);	
+
+//Otorgando el resultado del arreglo actualizado
+for(i=0;i<listadoStrings.length;i++)
+{
+	console.log(listadoStrings[i]);
+	document.getElementById("resultado").innerHTML=document.getElementById("resultado").innerHTML+listadoStrings[i]+"<br>";
+}
 }
